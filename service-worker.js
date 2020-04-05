4efc851118accb4e208a7a5db9588f4cfaae61b8
axaysingh <35101850+axaysingh@users.noreply.github.com>
2020-04-05 09:17:06 +0000

    Deploying to gh-pages from  @ 666f5ae68d309bdb48e36b30fd201913f442c590 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3619476b9f57a9cbc2407d24cdf5ccf5.js"
+  "/precache-manifest.1a28924f27a06db4b2af892bd08f9204.js"
 );
 
 self.addEventListener('message', (event) => {
