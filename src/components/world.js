c6977e5b601d65066734937277b9841a85fd8e6a
Akshay SIngh <42666383+yudizaxay@users.noreply.github.com>
2020-04-06 20:04:28 +0530

    desgin bug fixed!

--- src/components/world.js
+++ src/components/world.js
@@ -40,9 +40,9 @@ class World extends Component {
         <tr style={{textTransform:'uppercase'}}>
             {/* <td><img src={ratesArr[key].countryInfo.flag} height='20' width='25' alt="flag"/></td> */}
             <td class="state">{ratesArr[key].country} <img src={ratesArr[key].countryInfo.flag} height='10' width='20' alt="flag"/></td>
-            <td class="state" style={{background: 'rgb(248, 249, 250'}}>{ratesArr[key].cases}</td>
-            <td class="state">{ratesArr[key].recovered}</td>
             <td class="state" style={{background: 'rgb(248, 249, 250'}}>{ratesArr[key].active}</td>
+            <td class="state" >{ratesArr[key].cases}</td>
+            <td class="state" style={{background: 'rgb(248, 249, 250'}}>{ratesArr[key].recovered}</td>
             <td class="state">{ratesArr[key].deaths}</td>
         </tr>
         );
@@ -64,15 +64,70 @@ class World extends Component {
         return (
           <main>
             <div className="chat" style={{marginLeft:'-4%'}}>
-              <table style={{textTransform:'lowercase',margin:'0 auto'}}>
+              <table style={{margin:'0 auto'}}>
                 <thead>
                   <tr>
                     {/* <th>Flag</th> */}
-                    <th className="sticky">Country</th>
-                    <th className="sticky">Confirm</th>
-                    <th className="sticky">Recover</th>
-                    <th className="sticky">Active</th>
-                    <th className="sticky">Death</th>
+                    <th className="sticky">
+                      <abbr
+                        className={`${window.innerWidth <= 769 ? 'is-grey' : ''}`}
+                        title="Country"
+                      >
+                        {window.innerWidth <= 769
+                          ? window.innerWidth <= 375
+                            ? 'Country'
+                            : 'Country'
+                          : 'Country'}
+                      </abbr>
+                    </th>
+                    <th className="sticky">
+                      <abbr
+                        className={`${window.innerWidth <= 769 ? 'is-yellow' : ''}`}
+                        title="Confirm"
+                      >
+                        {window.innerWidth <= 769
+                          ? window.innerWidth <= 375
+                            ? 'C'
+                            : 'Confirm'
+                          : 'Confirmed'}
+                      </abbr>
+                    </th>                    
+                    <th className="sticky">
+                      <abbr
+                        className={`${window.innerWidth <= 769 ? 'is-blue' : ''}`}
+                        title="Active"
+                      >
+                        {window.innerWidth <= 769
+                          ? window.innerWidth <= 375
+                            ? 'A'
+                            : 'Active'
+                          : 'Active'}
+                      </abbr>
+                    </th>
+                    <th className="sticky">
+                      <abbr
+                        className={`${window.innerWidth <= 769 ? 'is-green' : ''}`}
+                        title="Recover"
+                      >
+                        {window.innerWidth <= 769
+                          ? window.innerWidth <= 375
+                            ? 'R'
+                            : 'Recover'
+                          : 'Recovered'}
+                      </abbr>
+                    </th>
+                    <th className="sticky">
+                    <abbr
+                      className={`${window.innerWidth <= 769 ? 'is-cherry' : ''}`}
+                      title="Death"
+                    >
+                      {window.innerWidth <= 769
+                        ? window.innerWidth <= 375
+                          ? 'D'
+                          : 'Death'
+                        : 'Deaths'}
+                    </abbr>
+                    </th>
                   </tr>
                 </thead>
                 {this.createTable()}
